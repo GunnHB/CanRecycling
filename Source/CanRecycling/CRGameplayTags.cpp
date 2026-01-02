88d5aa03099997893226b103f05e169474d0c55b
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2026-01-02 17:04:57 +0900

    [CanRecycling] CRGameplayTags 추가

---
+++ Source/CanRecycling/CRGameplayTags.cpp
@@ -0,0 +1,8 @@
+
+#include "CRGameplayTags.h"
+
+namespace CRGameplayTag
+{
+	CANRECYCLING_API UE_DEFINE_GAMEPLAY_TAG(InputTag_Throttle, "InputTag.Throttle");
+	CANRECYCLING_API UE_DEFINE_GAMEPLAY_TAG(InputTag_Brake, "InputTag.Brake");
+}
---
+++ Source/CanRecycling/CRGameplayTags.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace CRGameplayTag
+{
+	/* Input Tags */
+	CANRECYCLING_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Throttle);
+	CANRECYCLING_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Brake);
+}
