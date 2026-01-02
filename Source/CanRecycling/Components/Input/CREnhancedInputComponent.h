e3270f65883b04d651c168097d544f57338b801e
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2026-01-02 18:07:09 +0900

    [CanRecycling] CREnhancedInputComponent 바인딩 함수 추가

--- Source/CanRecycling/Components/Input/CREnhancedInputComponent.h
+++ Source/CanRecycling/Components/Input/CREnhancedInputComponent.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "EnhancedInputComponent.h"
+#include "GameplayTagContainer.h"
+#include "CanRecycling/DataAssets/Input/DataAsset_InputConfig.h"
 #include "CREnhancedInputComponent.generated.h"
 
+class UDataAsset_InputConfig;
 /**
  * 
  */
@@ -14,7 +17,17 @@ class CANRECYCLING_API UCREnhancedInputComponent : public UEnhancedInputComponen
 {
 	GENERATED_BODY()
 
-// public:
-// 	template<typename UserObject, typename CallbackFunc>
-// 	void BindNativeInputAction();
+public:
+	template<typename UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
 };
+
+template <typename UserObject, typename CallbackFunc>
+void UCREnhancedInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input config data asset is null. can not proceed with binding"));
+
+	UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag);
+	if (FoundAction != nullptr)
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+}
