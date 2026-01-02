1cf90ec9737105225695eb3a23c8eb0412fa83b5
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2026-01-02 15:48:30 +0900

    [CanRecycling] GoKart cpp 파일 추가

---
+++ Source/CanRecycling/Kart/CRSportKart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CRGoKartBase.h"
+#include "CRSportKart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CANRECYCLING_API ACRSportKart : public ACRGoKartBase
+{
+	GENERATED_BODY()
+	
+};
