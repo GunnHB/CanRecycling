384573770defbd0d6bb10af615b52ebc78b556d2
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2026-01-02 17:06:25 +0900

    [CanRecycling] CREnhancedInputComponent 추가

---
+++ Source/CanRecycling/Components/Input/CREnhancedInputComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "CREnhancedInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CANRECYCLING_API UCREnhancedInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+// public:
+// 	template<typename UserObject, typename CallbackFunc>
+// 	void BindNativeInputAction();
+};
