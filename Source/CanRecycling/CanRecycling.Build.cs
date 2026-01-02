c0cdd7270eea663e5b8d10da665192bf7f6d2f00
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2026-01-02 17:04:30 +0900

    [CanRecycling] Build.cs GameplayTag 모듈 추가

--- Source/CanRecycling/CanRecycling.Build.cs
+++ Source/CanRecycling/CanRecycling.Build.cs
@@ -8,6 +8,16 @@ public class CanRecycling : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles", "PhysicsCore" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"ChaosVehicles",
+			"PhysicsCore",
+			"GameplayTags",
+		});
 	}
 }
